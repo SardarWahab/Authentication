d85158c596aa4be37c950fa6239691167962cccc
SardarWahab <wahabsardar130@gmail.com>
2025-01-21 20:15:45 +0500

    `Refactored auth views and templates to use Django's built-in authentication system and message framework.`

--- auths/views.py
+++ auths/views.py
@@ -1,45 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
-from django.shortcuts import redirect
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-
+# Redirect user to the login page by default
 def login_view(request):
-        if request.method == 'POST':
-            form = AuthenticationForm(request, data=request.POST)
-            if form.is_valid():
-                username = form.cleaned_data.get('username')
-                password = form.cleaned_data.get('password')
-                user = authenticate(username=username, password=password)
-                if user is not None:
-                    auth_login(request, user)
-                    messages.success(request, f'Welcome {username}!')
-                    return redirect('home')
-                else:
-                    messages.error(request, 'Invalid username or password.')
+    if request.user.is_authenticated:  # If user is already logged in
+        return redirect('home')  # Redirect to home page
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                messages.success(request, f'Welcome {username}!')
+                return redirect('home')
             else:
                 messages.error(request, 'Invalid username or password.')
         else:
-            form = AuthenticationForm()
-        return render(request, 'auths/login.html', {'form': form})
+            messages.error(request, 'Invalid username or password.')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'auth/login.html', {'form': form})
 
 def register_view(request):
-        if request.method == 'POST':
-            form = UserCreationForm(request.POST)
-            if form.is_valid():
-                user = form.save()
-                auth_login(request, user)
-                messages.success(request, 'Registration successful.')
-                return redirect('home')
-            else:
-                messages.error(request, 'Unsuccessful registration. Invalid information.')
+    if request.user.is_authenticated:  # Redirect logged-in users to home
+        return redirect('home')
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            messages.success(request, 'Registration successful.')
+            return redirect('home')
         else:
-            form = UserCreationForm()
-        return render(request, 'auths/register.html', {'form': form})
+            messages.error(request, 'Unsuccessful registration. Invalid information.')
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth/register.html', {'form': form})
+
+@login_required
+def home(request):
+    return render(request, 'auth/home.html')  # Make sure this template exists
 
 def logout_view(request):
-        auth_logout(request)
-        messages.success(request, 'You have been logged out.')
-        return redirect('login')
+    auth_logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('login')
